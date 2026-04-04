fc42dcbfb9a45ce361db48f05f5f61515438a111
ericenlinea-netizen <ericenlinea@gmail.com>
2026-04-04 09:09:31 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ hora_entrada = None
 score_entrada = 0
 
 # ===== PARAMETROS ESTRATEGIA =====
-score_minimo_entrada = 9
+score_minimo_entrada = 7
 
 # ===== PARAMETROS DE TRADING REAL =====
 capital_inicial = 1000.0
